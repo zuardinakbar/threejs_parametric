c2a479bae7e181f5b82920e7c6b696d4fb679eb9
Zuardin Akbar <akbar.zuardin@gmail.com>
2023-11-16 11:46:57 +0100

    Initial commit

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ import GUI from 'lil-gui';
 //CONSTANT & VARIABLES
 let width = window.innerWidth;
 let height = window.innerHeight;
-//-- GUI PAREMETERS
- 
-//-- SCENE VARIABLES
+//-- GUI PARAMETERS
 var gui;
+const parameters = {
+  resolutionX: 3,
+  rotationX: 100
+}
+
+//-- SCENE VARIABLES
 var scene;
 var camera;
 var renderer;
@@ -21,15 +25,19 @@ var directionalLight;
 
 //-- GEOMETRY PARAMETERS
 //Create an empty array for storing all the cubes
- 
+let sceneCubes = [];
+let resX = parameters.resolutionX;
+let rotX = parameters.rotationX;
 
 function main(){
   //GUI
-    
+  gui = new GUI;
+  gui.add(parameters, 'resolutionX', 1, 10, 1);
+  gui.add(parameters, 'rotationX', 0, 180);
 
   //CREATE SCENE AND CAMERA
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 35, width / height, 0.1, 100);
+  camera = new THREE.PerspectiveCamera( 15, width / height, 0.1, 100);
   camera.position.set(10, 10, 10)
 
   //LIGHTINGS
@@ -44,7 +52,8 @@ function main(){
 
   //GEOMETRY INITIATION
   // Initiate first cubes
-
+  createCubes();
+  rotateCubes();
 
   //RESPONSIVE WINDOW
   window.addEventListener('resize', handleResize);
@@ -66,6 +75,67 @@ function main(){
 //-----------------------------------------------------------------------------------
 //HELPER FUNCTIONS
 //-----------------------------------------------------------------------------------
+//GEOMETRY FUNCTIONS
+// Create Cubes
+function createCubes(){
+  for(let i=0; i<resX; i++){
+    const geometry = new THREE.BoxGeometry(0.1, 1,1);
+    const material = new THREE.MeshPhysicalMaterial();
+    material.color = new THREE.Color(0xffffff);
+    material.color.setRGB(0,0,Math.random());
+
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(i*0.1, 0, 0);
+    cube.name = "cube " + i;
+    sceneCubes.push(cube);
+
+    scene.add(cube);
+  }
+}
+
+//Rotate Cubes
+function rotateCubes(){
+  sceneCubes.forEach((element, index)=>{
+    let scene_cube = scene.getObjectByName(element.name);
+    let radian_rot = (index*(rotX/resX)) * (Math.PI/180);
+    scene_cube.rotation.set(radian_rot, 0, 0)
+    rotX = parameters.rotationX;
+  })
+}
+
+//Remove 3D Objects and clean the caches
+function removeObject(sceneObject){
+  if (!(sceneObject instanceof THREE.Object3D)) return;
+
+  //Remove the geometry to free GPU resources
+  if(sceneObject.geometry) sceneObject.geometry.dispose();
+
+  //Remove the material to free GPU resources
+  if(sceneObject.material){
+    if (sceneObject.material instanceof Array) {
+      sceneObject.material.forEach(material => material.dispose());
+    } else {
+        sceneObject.material.dispose();
+    }
+  }
+
+  //Remove object from scene
+  sceneObject.removeFromParent();
+}
+
+//Remove the cubes
+function removeCubes(){
+  resX = parameters.resolutionX;
+  rotX = parameters.rotationX;
+
+  sceneCubes.forEach(element =>{
+    let scene_cube = scene.getObjectByName(element.name);
+    removeObject(scene_cube);
+  })
+
+  sceneCubes = [];
+}
+
 //RESPONSIVE
 function handleResize() {
   width = window.innerWidth;
@@ -76,10 +146,22 @@ function handleResize() {
   renderer.render(scene, camera);
 }
 
+
 //ANIMATE AND RENDER
 function animate() {
 	requestAnimationFrame( animate );
  
+  control.update();
+
+  if(resX != parameters.resolutionX){
+    removeCubes();
+    createCubes();
+    rotateCubes();
+  }
+
+  if (rotX != parameters.rotationX){
+    rotateCubes();
+  }
  
 	renderer.render( scene, camera );
 }
