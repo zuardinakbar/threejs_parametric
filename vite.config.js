0aa82ef0f64e5c34f8c0a2419eb759408099730e
Zuardin Akbar <akbar.zuardin@gmail.com>
2023-12-10 22:40:02 +0100

    update build

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/threejs_parametric/"
+  base: "/threejs_parametric/",
+  build: {
+    sourcemap: true,
+  }
 })
